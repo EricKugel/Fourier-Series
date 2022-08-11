e5f33f2226346ca5ec397ccd87f6a86cace95f8f
EricKugel <erickugel713@gmail.com>
2022-08-11 00:23:07 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fourier Series
+
+This is an animation of a Fourier Series. I'm not really sure how it works, I just know how to make it.
---
+++ scripts/main.js
@@ -0,0 +1,77 @@
+var canvas;
+var ctx;
+const FPS = 24;
+
+var circles = [];
+var colors = ["red", "orange", "yellow", "green", "blue", "purple", "black"]
+
+class Circle {
+    // speed is in radians per tick
+    constructor(x, y, speed, size, children) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.speed = speed;
+        this.point_x = this.x + this.size;
+        this.point_y = this.y;
+        this.theta = 0;
+        this.child = null;
+        this.children = children;
+        if (children > 0) {
+            this.child = new Circle(this.point_x, this.point_y, this.speed * 2, this.size / 2, children - 1);
+        }
+    }
+
+    update(coords = null) {
+        if (coords != null) {
+            this.x = coords[0];
+            this.y = coords[1];
+        }
+        this.theta += this.speed
+        if (this.theta > 2 * Math.PI) {
+            this.theta = this.theta % Math.PI;
+        }
+        this.point_x = this.x + Math.cos(this.theta) * this.size;
+        this.point_y = this.y - Math.sin(this.theta) * this.size;
+        if (this.child != null) {
+            this.child.update([this.point_x, this.point_y]);
+        }
+    }
+
+    draw() {
+        ctx.strokeStyle = colors[this.children % colors.length];
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.point_x, this.point_y);
+        ctx.stroke();
+        if (this.child != null) {
+            this.child.draw();
+        }
+    }    
+}
+
+window.onload = function() {
+    canvas = document.getElementById("canvas");
+    canvas.setAttribute("width", window.innerWidth);
+    canvas.setAttribute("height", window.innerHeight);
+    ctx = canvas.getContext("2d");
+    main();
+}
+
+function main() {
+    debugger;
+    circles.push(new Circle(window.innerWidth / 2, window.innerHeight / 2, Math.PI * 2 / (FPS * 4), 250, 5));
+    tick();
+}
+
+function tick() {
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    for (var i = 0; i < circles.length; i++) {
+        circles[i].update();
+        circles[i].draw();
+    }
+    setTimeout(tick, 40);
+}
