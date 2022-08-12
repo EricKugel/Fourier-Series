c9c30988bfe5329d75d1bcb977f67e8a03bf141f
Eric Kugel <68514820+EricKugel@users.noreply.github.com>
2022-08-12 17:32:00 +0000

    yup.

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,55 @@
 var canvas;
 var ctx;
 const FPS = 24;
+const SIZE = 50
 
 var circles = [];
 var colors = ["red", "orange", "yellow", "green", "blue", "purple", "black"]
+var circles = [
+  {
+    radius: 1,
+    theta: Math.PI / 2,
+    frequency: 0
+  },
+  {
+    radius: 1,
+    theta: 0,
+    frequency: 1,
+  },
+  {
+    radius: 1,
+    theta: 0,
+    frequency: -1
+  },
+  {
+    radius: -1/2,
+    theta: 0,
+    frequency: 2
+  },
+  {
+    radius: 1/2,
+    theta: 0,
+    frequency: -2
+  }
+]
 
 class Circle {
     // speed is in radians per tick
-    constructor(x, y, speed, size, children) {
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.speed = speed;
-        this.point_x = this.x + this.size;
-        this.point_y = this.y;
-        this.theta = 0;
-        this.child = null;
-        this.children = children;
-        if (children > 0) {
-            this.child = new Circle(this.point_x, this.point_y, this.speed * 2, this.size / 2, children - 1);
+    constructor(index) {
+        this.theta = circles[index].theta;
+        if (index = 0) {
+          this.x = screen.innerWidth / 2;
+          this.y = screen.innerHeight / 2;
+        } else {
+          this.x = circles[index - 1].circle.point_x;
+          this.y = circles[index - 1].circle.point_y
         }
+        this.radius = circles[index].radius * SIZE;
+        this.speed = (circles[index].frequency * Math.PI * 2) / FPS;
+        this.point_x = this.x + Math.cos(this.theta)
+        this.point_y = this.y + Math.sin(this.theta);
+        this.index = index;
+        circles[index].circle = this;
     }
 
     update(coords = null) {
@@ -62,8 +92,7 @@ window.onload = function() {
 }
 
 function main() {
-    debugger;
-    circles.push(new Circle(window.innerWidth / 2, window.innerHeight / 2, Math.PI * 2 / (FPS * 4), 250, 5));
+    circles.push(new Circle(window.innerWidth / 2, window.innerHeight / 2, Math.PI * 2 / (FPS * 4), 50, 5));
     tick();
 }
 
